b9add9c4a069097ee8c0892f781a3ce83152e684
mzaneri <michaelzaneri@gmail.com>
2019-04-29 14:04:14 -0400

    initial commit for basic implimentation of git in rust, arguement parsing and error handling completed  (hopeflly) idiomatically

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+use basicgrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguements: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = basicgrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
